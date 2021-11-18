8e4448fd616ee0f90ffdf508bd32fcc2ff897cac
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-11-18 16:16:33 -0300

    Remove old Container from login page styles

--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -1,24 +1,6 @@
 import styled from "styled-components";
 import { Button } from "../../components/Button";
 
-const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	width: 100vw;
-	height: 100vh;
-	align-items: center;
-	background-color: #6d7ce4;
-
-	padding: 100px 25px;
-
-	h1 {
-		font-weight: 500;
-		font-size: 28px;
-		color: white;
-		margin-bottom: 40px;
-	}
-`;
-
 const LoginButton = styled(Button)`
 	width: 237px;
 	height: 56px;
@@ -30,4 +12,4 @@ const RegisterButton = styled(Button)`
 	background-color: transparent;
 `;
 
-export { Container, LoginButton, RegisterButton };
+export { LoginButton, RegisterButton };
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,7 +1,24 @@
+import styled from "styled-components";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { PageContainer } from "../../components/PageContainer";
+
 export default function Register() {
 	return (
-		<div>
-			<h1>Register</h1>
-		</div>
+		<PageContainer>
+			<h1>Bem vinde ao GratiBox</h1>
+			<Input type="text" placeholder="Nome" />
+			<Input type="email" placeholder="Email" />
+			<Input type="password" placeholder="Senha" />
+			<Input type="password" placeholder="Confirmar senha" />
+			<RegisterButton type="submit" children="Cadastrar" />
+		</PageContainer>
 	);
 }
+
+const RegisterButton = styled(Button)`
+	width: 237px;
+	height: 56px;
+	font-size: 36px;
+	margin-top: 100px;
+`;
