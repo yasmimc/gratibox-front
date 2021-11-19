c2c6515009ac82d769e3dd3d6de0f4ea325a5450
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-11-19 20:22:52 -0300

    Add sign in function, using API route

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,20 +1,114 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { PageContainer } from "../../components/PageContainer";
 import { LoginButton, RegisterButton } from "./styles";
 import { Input } from "../../components/Input";
 import routes from "../../routes/routes";
 import Welcome from "../../components/Welcome";
+import { useState } from "react";
+import API from "../../services/API/requests";
+import { Form } from "../../components/Form";
+import { emailRegex } from "../register/regexValidations";
+import InputErrorMsg from "../../components/InputErrorMsg";
 
 export default function Login() {
-	return (
-		<PageContainer>
-			<Welcome />
-			<Input type="email" placeholder="Email" />
-			<Input type="password" placeholder="Senha" />
-			<LoginButton type="submit" children="Login" />
-			<Link to={routes.register}>
-				<RegisterButton children="Ainda não sou grato" />
-			</Link>
-		</PageContainer>
-	);
+    const navigate = useNavigate();
+
+    const [input, setInput] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [inputError, setInputError] = useState({
+        email: null,
+        password: null,
+        emptyFiels: null,
+    });
+
+    const [loading, setLoading] = useState(false);
+
+    function submitSignIn(event) {
+        event.preventDefault();
+        setLoading(true);
+
+        const emptyFields = Object.values(input).some((key) => key === "");
+        if (emptyFields) {
+            setInputError({ ...inputError, emptyFields: true });
+            setLoading(false);
+            return;
+        }
+
+        const inputErrors = Object.values(inputError).some(
+            (key) => key === true
+        );
+        if (!inputErrors) {
+            API.signIn(input)
+                .then(() => {
+                    setLoading(false);
+                    navigate(routes.plans);
+                })
+                .catch((err) => {
+                    setLoading(false);
+                    if (err.response)
+                        setInputError({
+                            ...inputError,
+                            invalidCredentials: true,
+                        });
+                });
+        }
+    }
+
+    return (
+        <PageContainer>
+            <Welcome />
+            <Form onSubmit={submitSignIn}>
+                <Input
+                    type="email"
+                    placeholder="Email"
+                    onChange={(event) => {
+                        setInput({ ...input, email: event.target.value });
+                        setInputError({
+                            ...inputError,
+                            email:
+                                !event.target.value.match(emailRegex)?.length &&
+                                event.target.value !== "",
+                            invalidCredentials: false,
+                        });
+                    }}
+                />
+                {inputError.email ? (
+                    <InputErrorMsg
+                        children="
+                        Por favor, digite um email válido 🙏"
+                    />
+                ) : null}
+
+                <Input
+                    type="password"
+                    placeholder="Senha"
+                    onChange={(event) => {
+                        setInput({ ...input, password: event.target.value });
+                        setInputError({
+                            ...inputError,
+                            invalidCredentials: false,
+                        });
+                    }}
+                />
+                {inputError.emptyFields ? (
+                    <InputErrorMsg children="Por favor, preencha todos os campos 🙏" />
+                ) : null}
+
+                {inputError.invalidCredentials ? (
+                    <InputErrorMsg children="Credenciais inválidas. Por favor, tente novamente 🙏" />
+                ) : null}
+                <LoginButton
+                    disabled={loading}
+                    type="submit"
+                    children="Login"
+                />
+            </Form>
+            <Link to={routes.register}>
+                <RegisterButton children="Ainda não sou grato" />
+            </Link>
+        </PageContainer>
+    );
 }
--- src/services/API/requests.js
+++ src/services/API/requests.js
@@ -2,13 +2,20 @@ import axiosBase from "./axiosBase";
 
 const API = {
     signUp,
-    // signIn,
+    signIn,
     // validateToken,
 };
 
 function signUp({ name, email, password }) {
     return axiosBase.post("/sign-up", {
         name,
+        email,
+        password,
+    });
+}
+
+function signIn({ email, password }) {
+    return axiosBase.post("/sign-in", {
         email,
         password,
     });
