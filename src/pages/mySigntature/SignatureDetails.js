04ccfa2317e2d90635e51e408233611fabd23445
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-11-22 03:06:50 -0300

    Check if the next delivery is on a weekend and switch to the first business day

--- src/pages/mySigntature/SignatureDetails.js
+++ src/pages/mySigntature/SignatureDetails.js
@@ -2,6 +2,16 @@ import dayjs from "dayjs";
 import { Deliveries, Products } from "./styles";
 
 export default function SignatureDetails({ userPlan }) {
+    function businessDay(date) {
+        if (dayjs(date).day() === 6)
+            return dayjs(dayjs(date).add(2, "days").format());
+
+        if (dayjs(date).day() === 0)
+            return dayjs(dayjs(date).add(1, "days").format());
+
+        return dayjs(date).format();
+    }
+
     return (
         <>
             <Deliveries>
@@ -11,20 +21,28 @@ export default function SignatureDetails({ userPlan }) {
                 </p>
                 <p>
                     <span className="label">Data de assinatura: </span>
-                    {dayjs(userPlan?.date).format("DD/MM/YYYY")}
+                    {dayjs(businessDay(userPlan?.date)).format("DD/MM/YYYY")}
                 </p>
                 <p>
                     <span className="label">Próximas entregas:</span>
                     <li>{dayjs(userPlan?.startDate).format("DD/MM/YYYY")}</li>
                     <li>
-                        {dayjs(userPlan?.startDate)
-                            .add(userPlan?.planPeriod, "days")
-                            .format("DD/MM/YYYY")}
+                        {dayjs(
+                            businessDay(
+                                dayjs(userPlan?.startDate)
+                                    .add(userPlan?.planPeriod, "days")
+                                    .format()
+                            )
+                        ).format("DD/MM/YYYY")}
                     </li>
                     <li>
-                        {dayjs(userPlan?.startDate)
-                            .add(userPlan?.planPeriod * 2, "days")
-                            .format("DD/MM/YYYY")}
+                        {dayjs(
+                            businessDay(
+                                dayjs(userPlan?.startDate)
+                                    .add(userPlan?.planPeriod * 2, "days")
+                                    .format()
+                            )
+                        ).format("DD/MM/YYYY")}
                     </li>
                 </p>
             </Deliveries>
